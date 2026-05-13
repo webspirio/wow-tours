6b52f8cd271a2eb819d05eacd813775e14bd6e99
oleksandr-jr <oleksandr.cho@gmail.com>
2026-05-13 15:48:07 +0200

    feat: add Tailwind v4 theme tokens + WOW palette

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './styles/globals.css'
+import Landing from './landing.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Landing />
+  </React.StrictMode>
+)
